8789e9508d9aa0c5a094b786eac2e14a3ea220ee
Yudi Fathurrahman <yfathurrahman@gmail.com>
2021-10-03 17:52:33 +0700

    update consumer data

--- src/components/consumer/Data.vue
+++ src/components/consumer/Data.vue
@@ -3,59 +3,51 @@
     <div class="container mt-4">
       <div class="card">
         <div class="card-box">
-          <div class="feed mt-2">
-            <div class="text-justify">
-              <div class="row">
-                <div class="col-1">
-                  <img
-                    src="../../assets/logo.png"
-                    alt="user-img"
-                    class="
-                      img-fluid img-thumbnail
-                      rounded-circle
-                      mb-2
-                      dashboard-profile
-                    "
-                  />
-                </div>
-                <div class="ml-3 col-8 pt-2">SMPN 10 TAPUNG</div>
-              </div>
-               <table border="1">
-       <tr v-for="(alumni,index) in alumnies" :key="index">
-         <td>{{alumni.id_pelajar}}</td>
-       </tr>
-       tasdasd
-     </table>
-              <div class="ml-5 text-muted">
-              10:12, 27 januari 2021, 
-              </div>
-              <div class="mr-5 ml-5">
-                Salam hormat bagi seluruh alumni, dan juga pengguna alumni STAI
-                Auliaurrasyidin Informasi data dalam tracer alumni ini akan
-                sangat bermanfaat bagi universitas dalam rangka mengevaluasi,
-                memperbaiki, dan meningkatkan kualitas kampus STAI
-                Auliaurrasyidin dalam hal fasilitas, kurikulum dan juga
-                pembelajaran. Selain itu, hasil tracer study juga sangat berguna
-                untuk pengisian borang dalam rangka akreditasi program studi dan
-                perguruan tinggi. serta dapat memberikan informasi penting
-                mengenai hubungan antara dunia pendidikan tinggi dengan dunia
-                usaha dan industri. Tracer Alumni ini dapat menyajikan informasi
-                mendalam dan rinci mengenai kecocokan kerja yang terjadi. Dengan
-                demikian, Tracer Alumni dapat digunakan untuk membantu mengatasi
-                permasalahan kesenjangan kesempatan kerja dan upaya dari
-                perbaikannya. Kami sangat berharap kesediaan para alumni, dan
-                juga pengguna alumni STAI Auliaurrasyidin, untuk meluangkan
-                waktu mengisi kuisioner tracer alumni dengan sebenar-benarnya.
-                Kami menjamin kerahasiaan informasi yang diisikan dalam
-                kuisioner tersebut. Kami menyampaikan terima kasih atas dukungan
-                dan partisiasi saudara.
-              </div>
-            </div>
-            <hr />
-          </div>
-        </div>
-      </div>
-    </div>
+          <h4 class="header-title my-3">Data Alumni Consumer</h4>
+          <div class="contain mb-3">
+            <table border="1">
+            <tr>
+            <table class="table table-vcenter card-table">
+                      <thead>
+                        <tr>
+                          <th>Name</th>
+                          <th>Title</th>
+                          <th>Role</th>
+                          <th class="w-1"></th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>
+                            <div class="d-flex py-1 align-items-center">
+                              <span class="avatar me-2" style="background-image: url(./static/avatars/006m.jpg)"></span>
+                              <div class="flex-fill">
+                                <div class="font-weight-medium">Lorry Mion</div>
+                                <div class="text-muted"><a href="#" class="text-reset">lmiona@livejournal.com</a></div>
+                              </div>
+                            </div>
+                          </td>
+                          <td>
+                            <div>Automation Specialist IV</div>
+                            <div class="text-muted">Accounting</div>
+                          </td>
+                          <td class="text-muted">
+                            User
+                          </td>
+                          <td>
+                            <a href="#">Edit</a>
+                          </td>
+                        </tr>
+                        
+              <tr v-for="(alumni,index) in alumnies" :key="index">
+                <td>{{alumni.id_pelajar}}</td>
+              </tr>
+                      </tbody>
+                    </table>
+            
+         </div>
+         </div>
+         </div>
 </template>
 <script>
 import axios from "axios";
@@ -65,7 +57,7 @@ export default {
     let alumnies = ref([]);
     onMounted(() => {
       axios
-        .get("http://127.0.0.1:8002/alumni", {
+        .get("http://127.0.0.1:8002/alumni/kerja", {
           headers: {
             "app-origins": "yes",
           },
